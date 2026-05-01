971e6b98723db96c8edce3a341fad3666b4faa9d
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-05-01 22:32:38 +0800

    uhm errors dealing

---
+++ Globals/MusicManager/music_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@onready var musics : Dictionary = {
+	
+}
+
+func play_song(song: String, starting_point: float = 0.0) -> void: ## Stops the current song playing and plays the selected song
+	if not is_song_playing(song):
+		stop_all_songs()
+		musics[song].play(starting_point)
+
+func stop_all_songs() -> void: ## Stops all songs
+	for song in musics.values():
+		if song is AudioStreamPlayer:
+			
+			if song.playing:
+				song.stop()
+
+func is_song_playing(song: String) -> bool:
+	return musics[song].playing
